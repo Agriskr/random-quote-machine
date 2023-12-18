27c994a21cfba20bdb59d768b6133379ea5a9b7d
Agris Krumbergs <agriskrum@gmail.com>
2023-12-18 19:19:04 +0200

    random-quote-machine_first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { CiTwitter } from 'react-icons/ci';
+
 
 function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({});
+
+  useEffect(() => {
+    getQuote();
+  }, [])
+
+  const getQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => { return response.json() })
+      .then((data) => {
+        setQuoteInfo({
+          text: data.content,
+          author: data.author,
+        });
+
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-box" id="quote-box">
+        <p className="quote-text" id="text">{quoteInfo.text}</p>
+        <p className="quote-author" id="author">{quoteInfo.author}</p>
+        <div className='buttons'>
+          <button className="button" id="new-quote" onClick={getQuote}>
+            New Quote</button>
+          <a className="button" href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo.text} id="tweet-quote"><CiTwitter /></a>
+        </div>
+      </div>
+      <div className="footer">by AK</div>
     </div>
   );
+
 }
+
 
 export default App;
